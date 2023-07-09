eb753df03cbf83b46546063a45a2bd5bca0d26e9
umuttop <84898565+umuttop@users.noreply.github.com>
2023-07-09 17:20:10 +0200

    NewsPage + Farben
    Füllen der NewsPage mit Content, änderung der
    Farben

--- src/pages/NewsPage.vue
+++ src/pages/NewsPage.vue
@@ -1,47 +1,196 @@
-<!-- Main.vue -->
 <template>
-  <div>
-    <blog-post
-      v-for="post in blogPosts"
-      :key="post.id"
-      :title="post.title"
-      :truncatedText="post.truncatedText"
-      :fullText="post.fullText"
-    ></blog-post>
-  </div>
+  <body>
+    <div class="wrapper">
+      <div class="zero"></div>
+      <div class="one">
+        <div class="image"><img src="~assets/Fachschaft.jpg"></div>
+        <h2>Über Uns</h2>
+        Die Fachschaftsvertretung ist die offizielle Vertretung der Studierenden
+        am Fachbereich. Sie wird einmal pro akademischem Jahr (2 Semester)
+        gewählt und fungiert als Bindeglied zwischen den Studierenden,
+        Professoren und dem Fachbereich. Da die Fachschaftsvertretung auf
+        basisdemokratischen Prinzipien beruht, hat jeder Studierende am
+        Fachbereich Wirtschaft die Möglichkeit, sich in der
+        Fachschaftsvertretung zu engagieren. Selbst wenn die Wahl nicht
+        erfolgreich ist oder man nur gelegentlich Zeit hat, kann man sich gerne
+        als Helfer/in bei uns melden und aktiv am Geschehen teilnehmen.
+        <h4>Wir helfen bei euren Anliegen</h4>
+        <p>
+          Wir bieten umfassende Informationen zur Fachhochschule an und geben
+          wertvolle Tipps speziell für Erstsemester. Wenn du Schwierigkeiten bei
+          der Klausuranmeldung hast, unterstützen wir dich gerne dabei. Außerdem
+          freuen wir uns, wenn du Gedächtnisprotokolle von Klausuren erstellst
+          und sie mit uns teilst. Ein Pool von Gedächtnisprotokollen ist für
+          andere Studierende sehr hilfreich, um sich optimal auf Klausuren
+          vorzubereiten. Wir sind die Vermittler zwischen den Studierenden, den
+          Professoren und dem Fachbereich. Zögere nicht, uns anzusprechen, wenn
+          du Fragen oder Anliegen hast.
+        </p>
+        <h4>Kontakt</h4>
+        <p>
+          Falls du Fragen zu uns oder unserer Arbeit hast, besuche uns gerne
+          persönlich im Raum 0.12 in C03 jeden Mittwoch und Donnerstag zwischen
+          12:00 und 14:00 Uhr. Und wenn wir mal nicht vor Ort sind, kannst du
+          uns jederzeit über
+          <a
+            href=" https://www.instagram.com/fawi_kiel/"
+            style="color: rgba(128, 207, 226, 0.938)"
+            target="_blank"
+            >Instagram</a
+          >,
+          <a
+            href="https://www.facebook.com/fawi.kiel/"
+            style="color: rgba(128, 207, 226, 0.938)"
+            target="_blank"
+          >
+            Facebook </a
+          >oder per
+          <a
+            href="mailto:wirtschaft.fachschaft@fh-kiel.de"
+            style="color: rgba(128, 207, 226, 0.938)"
+            >Mail.</a
+          >
+        </p>
+      </div>
+      <div class="two">
+        <h2>Das IBS Team sucht Studentische Hilfskraft</h2>
+
+        <p>
+          Das IBS Büro der Fachhochschule Kiel sucht ab sofort eine studentische Hilfskraft für eine Arbeitszeit von mindestens 30 Stunden pro Monat. Wir sind offen für Bewerbungen von Studierenden in den unteren Semestern, da wir an einer langfristigen Zusammenarbeit interessiert sind.
+
+Die Aufgaben umfassen allgemeine Verwaltungsaufgaben, die Organisation von Veranstaltungen, Mitarbeit bei Messen und die Pflege von Datenbanken.
+
+Neben Eigenschaften wie Kontaktfreudigkeit, sozialer Kompetenz, guten Umgangsformen und Zuverlässigkeit sind insbesondere folgende Kenntnisse und Fähigkeiten erforderlich:
+<ul>
+<li>Sicherer Umgang mit MS Office</li>
+<li>Schnelle Auffassungsgabe und Organisationstalent</li>
+<li>Gute Deutschkenntnisse</li>
+<li>Kommunikationsfreude, Spaß am Umgang mit Menschen und Humor</li>
+</ul>
+Das Büro für das Industriebegleitete Studium (IBS) ist verantwortlich für die Koordination des dualen Studienangebots in den Bereichen Maschinenwesen, Informatik & Elektrotechnik, Bauingenieurwesen und Wirtschaft.
+Weitere Informationen zum Industriebegleiteten Studium finden Sie unter <a
+            href="http://www.fh-kiel.de/ibs"
+            style="color: rgba(128, 207, 226, 0.938)"
+            >http://www.fh-kiel.de/ibs</a
+          >.
+Bitte senden Sie Ihre Bewerbung per E-Mail an <a
+            href="mailto:claudia.martens@fh-kiel.de"
+            style="color: rgba(128, 207, 226, 0.938)"
+            >claudia.martens@fh-kiel.de</a>.
+        Bei Fragen steht Ihnen Frau Martens telefonisch unter Tel. <u>0431 / 210-2605</u> zur Verfügung.
+        </p>
+      </div>
+      <div class="three">
+        <h2>Kunst und Kultur auf dem Campus der FH außerhalb der Vorlesungszeiten</h2>
+        <p>
+Auf dem Campus der Fachhochschule Kiel gibt es nicht nur Lehrveranstaltungen und Experimente, sondern auch eine Vielzahl von Kultureinrichtungen.
+Insbesondere während der bevorstehenden Semesterferien lohnt sich ein Besuch. Wir stellen Ihnen fünf Einrichtungen vor, die einen Besuch wert sind.</p>
+<p>Die 360-Grad-Kuppel mit ihrem vielfältigen Programm an immersiven Erlebnissen mit beeindruckenden Projektionen und erstklassigem Sound ist wohl die
+bekannteste Kultureinrichtung der Fachhochschule Kiel. Ob Astronomie und Wissenschaft, Kultur und Unterhaltung, Programme für Kinder und Senioren oder spannende
+Vorträge über das Universum - die Kulturhalbkugel bietet für verschiedene Interessensgebiete etwas Besonderes.</p><p>Hier sind einige Beispiele: Am 7. Juli und am 4. August
+um 20 Uhr widmet sich die Veranstaltungsreihe "Allein im All?" der Suche nach außerirdischem Leben im Universum. Der Eintritt beträgt 11 Euro pro Person, ermäßigt 9 Euro.</p><p>
+Für diejenigen, die lieber irdischen Freuden nachgehen möchten, bietet die wöchentliche Veranstaltung "Queen Heaven" eine Hommage an die legendäre britische Band Queen.
+Unter der Kuppel werden Songs wie "Bohemian Rhapsody" und "Don't Stop Me Now" mit Originalaufnahmen aus ihrer glorreichen Zeit abgespielt - ein Gänsehauterlebnis.
+Der Eintritt kostet 12 Euro, ermäßigt 10 Euro. Das vollständige, quartalsweise wechselnde Programm finden Sie auf der Webseite des Mediendoms.</p><p>
+Während wir heutzutage einen Hochleistungsrechner in unserer Hosentasche haben, vergessen wir oft, wie alles begonnen hat. Der Z11, eine der ersten serienmäßig
+produzierten elektronischen Rechenanlagen der Welt aus dem Jahr 1955, ist ein beeindruckendes Stück deutscher Computergeschichte.
+Das Computermuseum der Fachhochschule Kiel beherbergt ein Originalmodell dieses Rechners sowie eine Vielzahl anderer Exponate, von mechanischen Rechenmaschinen vergangener
+Jahrhunderte über Großrechenanlagen der 1950er bis 1980er Jahre bis hin zu PCs und Notebooks der späten 1990er Jahre. Das Museum ist nicht nur für Technikbegeisterte und
+Nostalgiker ein absolutes Muss.<br>
+Öffnungszeiten: Samstag und Sonntag von 14 bis 18 Uhr (letzter Einlass um 17 Uhr)
+Eintrittspreis: 6 Euro, ermäßigt 4,50 Euro </p><p>
+An jedem ersten Samstag im Monat um 15:15 Uhr besteht die Möglichkeit, an einer Führung durch das Museum teilzunehmen.
+Das Industriemuseum Howaldtsche Metallgießerei ist das letzte erhaltene Gebäude der Maschinenfabrik der Howaldtswerke, die in den 1880er Jahren errichtet wurde.
+Hier können Besucher einen Eindruck von der harten Arbeit in der Werft vor über 100 Jahren bekommen. Neben den ausgestellten Öfen, Werkbänken und Werkzeugen gibt
+es eine Fotoausstellung zum Neubeginn der Werft nach 1945. Es werden auch Workshops und Führungen angeboten. <br>Die Öffnungszeiten sind dienstags und sonntags von 10
+bis 18 Uhr. Der Eintritt zu den Ausstellungen, Workshops und Führungen ist kostenfrei. </p><p>
+Der Bunker-D auf dem Campus der Fachhochschule Kiel erinnert an die dunkelste Zeit der deutschen und Kieler Geschichte. In diesem Zweiten Weltkriegsbunker werden wechselnde
+Kunstwerke aus den Bereichen Kunst im öffentlichen Raum, Bildhauerei, Plastik, Grafik, Malerei, Fotografie und Medienkunst ausgestellt. Die Kunstwerke sind speziell für
+diesen Ort geschaffen worden und stehen unter einem besonderen Eindruck. Die CampusKunst-D ist nicht nur im Bunker-D, sondern im gesamten Campus verteilt. Informationen
+finden Sie auf der <a href="https://www.fh-kiel.de/bunker-d/" style="color: rgba(128, 207, 226, 0.938)">Website</a>.<br>
+Der Bunker beherbergt nicht nur die meisten Kunstwerke der CampusKunst-D, sondern dient auch als Kultur- und Kommunikationszentrum. In der Galerie des Bunkers findet
+derzeit die Sonderausstellung "FASIYA, Ukrainische Kunst im Bunker-D" statt, bei der Werke von 20 zeitgenössischen ukrainischen Künstlerinnen präsentiert werden.
+Die Ausstellung bietet den Besucherinnen die Möglichkeit, die künstlerische Vielfalt des Landes jenseits des kriegerischen Konflikts zu entdecken.
+Nach dem Ausstellungsbesuch lädt das Café im Bunker zum gemütlichen Beisammensein bei Kaffee und Muffins ein. Der Bunker-D bietet ein Kunsterlebnis der besonderen Art.<br>
+Öffnungszeiten Galerie und Café: Montag bis Freitag von 10 bis 14 Uhr, Mittwoch 10 bis 22 Uhr!
+        </p>
+      </div>
+    </div>
+  </body>
 </template>
-
 <script>
-import BlogPost from "@/components/BlogPost.vue";
-
-export default {
-  components: {
-    BlogPost,
-  },
-  data() {
-    return {
-      blogPosts: [
-        {
-          id: 1,
-          title: "Post 1",
-          truncatedText: "This is the truncated text of post 1...",
-          fullText: "This is the full text of post 1.",
-        },
-        {
-          id: 2,
-          title: "Post 2",
-          truncatedText: "This is the truncated text of post 2...",
-          fullText: "This is the full text of post 2.",
-        },
-        // Add more blog posts as needed
-      ],
-    };
-  },
-};
 </script>
 
-<style scoped>
-.blog-post {
-  margin-bottom: 20px;
+<style>
+@import url("https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800,900");
+body {
+  font-family: "Poppins", sans-serif;
+  background-color: #e8e8e8;
+}
+
+#head {
+  background-color: #474747;
+  display: inline-block;
+  align-content: center;
+  justify-content: center;
+  font-size: 100%;
+  color: white;
+  width: 100%;
+  height: 100px;
+}
+
+img {
+  width: 25%;
+  height: auto;
+  float: right;
+  margin-top: 5%;
+  margin-left: 2.5%;
+  margin-right: 5%;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-auto-rows: minmax(400px, 3);
+  grid-gap: 1%;
+  padding: 2%;
+  font: Arial;
+}
+
+.one {
+  border-radius: 50px;
+  padding: 2%;
+  grid-column: 4/ 4;
+  grid-row: 1 / 4;
+  background-color: #152565;
+  color: white;
+  font-size: 140%;
+}
+.two {
+  border-radius: 50px;
+  padding: 2%;
+  grid-column: 4 / 4;
+  grid-row: 5 / 9;
+  background-color: #616161;
+  color: rgb(255, 255, 255);
+  font-size: 140%;
+}
+.three {
+  border-radius: 50px;
+  padding: 2%;
+  grid-column: 4 / 4;
+  grid-row: 10/ 14;
+  background-color: #152565;
+  color: white;
+  font-size: 140%;
+}
+.four {
+  border-radius: 50px;
+  padding: 2%;
+  grid-column: 4 / 4;
+  grid-row: 15 / 19;
+  background-color: #616161;
+  color: rgb(255, 255, 255);
+  margin-bottom: 2%;
 }
 </style>
