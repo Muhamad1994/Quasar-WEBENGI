739abd62f2aace8a5d65701217adf50604e4669f
Muhamad1994 <131359879+Muhamad1994@users.noreply.github.com>
2023-07-14 22:53:48 +0200

    update

--- Kalender.js
+++ Kalender.js
@@ -1,217 +1,3 @@
-//global variables
-var monthEl = $(".c-main");
-var dataCel = $(".c-cal__cel");
-var dateObj = new Date();
-var month = dateObj.getUTCMonth() + 1;
-var day = dateObj.getUTCDate();
-var year = dateObj.getUTCFullYear();
-var monthText = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-var indexMonth = month;
-var todayBtn = $(".c-today__btn");
-var addBtn = $(".js-event__add");
-var saveBtn = $(".js-event__save");
-var closeBtn = $(".js-event__close");
-var winCreator = $(".js-event__creator");
-var inputDate = $(this).data();
-today = year + "-" + month + "-" + day;
-
-// ------ set default events -------
-function defaultEvents(dataDay, dataName, dataNotes, classTag) {
-  var date = $("*[data-day=" + dataDay + "]");
-  date.attr("data-name", dataName);
-  date.attr("data-notes", dataNotes);
-  date.addClass("event");
-  date.addClass("event--" + classTag);
-}
-
-defaultEvents(today, "YEAH!", "Today is your day", "important");
-defaultEvents(
-  "2022-12-25",
-  "MERRY CHRISTMAS",
-  "A lot of gift!!!!",
-  "festivity"
-);
-defaultEvents("2022-05-04", "LUCA'S BIRTHDAY", "Another gifts...?", "birthday");
-defaultEvents(
-  "2022-03-03",
-  "MY LADY'S BIRTHDAY",
-  "A lot of money to spent!!!!",
-  "birthday"
-);
-
-// ------ functions control -------
-
-//button of the current day
-todayBtn.on("click", function () {
-  if (month < indexMonth) {
-    var step = indexMonth % month;
-    movePrev(step, true);
-  } else if (month > indexMonth) {
-    var step = month - indexMonth;
-    moveNext(step, true);
-  }
-});
-
-//higlight the cel of current day
-dataCel.each(function () {
-  if ($(this).data("day") === today) {
-    $(this).addClass("isToday");
-    fillEventSidebar($(this));
-  }
-});
-
-//window event creator
-addBtn.on("click", function () {
-  winCreator.addClass("isVisible");
-  $("body").addClass("overlay");
-  dataCel.each(function () {
-    if ($(this).hasClass("isSelected")) {
-      today = $(this).data("day");
-      document.querySelector('input[type="date"]').value = today;
-    } else {
-      document.querySelector('input[type="date"]').value = today;
-    }
-  });
-});
-closeBtn.on("click", function () {
-  winCreator.removeClass("isVisible");
-  $("body").removeClass("overlay");
-});
-saveBtn.on("click", function () {
-  var inputName = $("input[name=name]").val();
-  var inputDate = $("input[name=date]").val();
-  var inputNotes = $("textarea[name=notes]").val();
-  var inputTag = $("select[name=tags]").find(":selected").text();
-
-  dataCel.each(function () {
-    if ($(this).data("day") === inputDate) {
-      if (inputName != null) {
-        $(this).attr("data-name", inputName);
-      }
-      if (inputNotes != null) {
-        $(this).attr("data-notes", inputNotes);
-      }
-      $(this).addClass("event");
-      if (inputTag != null) {
-        $(this).addClass("event--" + inputTag);
-      }
-      fillEventSidebar($(this));
-    }
-  });
-
-  winCreator.removeClass("isVisible");
-  $("body").removeClass("overlay");
-  $("#addEvent")[0].reset();
-});
-
-//fill sidebar event info
-function fillEventSidebar(self) {
-  $(".c-aside__event").remove();
-  var thisName = self.attr("data-name");
-  var thisNotes = self.attr("data-notes");
-  var thisImportant = self.hasClass("event--important");
-  var thisBirthday = self.hasClass("event--birthday");
-  var thisFestivity = self.hasClass("event--festivity");
-  var thisEvent = self.hasClass("event");
-
-  switch (true) {
-    case thisImportant:
-      $(".c-aside__eventList").append(
-        "<p class='c-aside__event c-aside__event--important'>" +
-          thisName +
-          " <span> • " +
-          thisNotes +
-          "</span></p>"
-      );
-      break;
-    case thisBirthday:
-      $(".c-aside__eventList").append(
-        "<p class='c-aside__event c-aside__event--birthday'>" +
-          thisName +
-          " <span> • " +
-          thisNotes +
-          "</span></p>"
-      );
-      break;
-    case thisFestivity:
-      $(".c-aside__eventList").append(
-        "<p class='c-aside__event c-aside__event--festivity'>" +
-          thisName +
-          " <span> • " +
-          thisNotes +
-          "</span></p>"
-      );
-      break;
-    case thisEvent:
-      $(".c-aside__eventList").append(
-        "<p class='c-aside__event'>" +
-          thisName +
-          " <span> • " +
-          thisNotes +
-          "</span></p>"
-      );
-      break;
-  }
-}
-dataCel.on("click", function () {
-  var thisEl = $(this);
-  var thisDay = $(this).attr("data-day").slice(8);
-  var thisMonth = $(this).attr("data-day").slice(5, 7);
-
-  fillEventSidebar($(this));
-
-  $(".c-aside__num").text(thisDay);
-  $(".c-aside__month").text(monthText[thisMonth - 1]);
-
-  dataCel.removeClass("isSelected");
-  thisEl.addClass("isSelected");
-});
-
-//function for move the months
-function moveNext(fakeClick, indexNext) {
-  for (var i = 0; i < fakeClick; i++) {
-    $(".c-main").css({
-      left: "-=100%",
-    });
-    $(".c-paginator__month").css({
-      left: "-=100%",
-    });
-    switch (true) {
-      case indexNext:
-        indexMonth += 1;
-        break;
-    }
-  }
-}
-function movePrev(fakeClick, indexPrev) {
-  for (var i = 0; i < fakeClick; i++) {
-    $(".c-main").css({
-      left: "+=100%",
-    });
-    $(".c-paginator__month").css({
-      left: "+=100%",
-    });
-    switch (true) {
-      case indexPrev:
-        indexMonth -= 1;
-        break;
-    }
-  }
-}
-
 function CalendarApp(date) {
   if (!(date instanceof Date)) {
     date = new Date();
@@ -240,7 +26,39 @@ function CalendarApp(date) {
     "Nov",
     "Dec",
   ];
-
+  this.quotes = [
+    "Whatever the mind of man can conceive and believe, it can achieve. –Napoleon Hill",
+    "Strive not to be a success, but rather to be of value. –Albert Einstein",
+    "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.  –Robert Frost",
+    "I attribute my success to this: I never gave or took any excuse. –Florence Nightingale",
+    "You miss 100% of the shots you don’t take. –Wayne Gretzky",
+    "The most difficult thing is the decision to act, the rest is merely tenacity. –Amelia Earhart",
+    "Every strike brings me closer to the next home run. –Babe Ruth",
+    "Definiteness of purpose is the starting point of all achievement. –W. Clement Stone",
+    "Life isn’t about getting and having, it’s about giving and being. –Kevin Kruse",
+    "Life is what happens to you while you’re busy making other plans. –John Lennon",
+    "We become what we think about. –Earl Nightingale",
+    "Life is 10% what happens to me and 90% of how I react to it. –Charles Swindoll",
+    "The most common way people give up their power is by thinking they don’t have any. –Alice Walker",
+    "The mind is everything. What you think you become.  –Buddha",
+    "Winning isn’t everything, but wanting to win is. –Vince Lombardi",
+    "Every child is an artist.  The problem is how to remain an artist once he grows up. –Pablo Picasso",
+    " You can never cross the ocean until you have the courage to lose sight of the shore. –Christopher Columbus",
+    "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel. –Maya Angelou",
+    "Either you run the day, or the day runs you. –Jim Rohn",
+    "Whether you think you can or you think you can’t, you’re right. –Henry Ford",
+    "The two most important days in your life are the day you are born and the day you find out why. –Mark Twain",
+    "Whatever you can do, or dream you can, begin it.  Boldness has genius, power and magic in it. –Johann Wolfgang von Goethe",
+    "The best revenge is massive success. –Frank Sinatra",
+    "People often say that motivation doesn’t last. Well, neither does bathing.  That’s why we recommend it daily. –Zig Ziglar",
+    "Life shrinks or expands in proportion to one’s courage. –Anais Nin",
+    "If you hear a voice within you say “you cannot paint,” then by all means paint and that voice will be silenced. –Vincent Van Gogh",
+    "There is only one way to avoid criticism: do nothing, say nothing, and be nothing. –Aristotle",
+    "Ask and it will be given to you; search, and you will find; knock and the door will be opened for you. –Jesus",
+    "The only person you are destined to become is the person you decide to be. –Ralph Waldo Emerson",
+    "Go confidently in the direction of your dreams.  Live the life you have imagined. –Henry David Thoreau",
+    "Few things can help an individual more than to place responsibility on him, and to let him know that you trust him.  –Booker T. Washington",
+  ];
   this.apts = [
     {
       name: "Finish this web app",
